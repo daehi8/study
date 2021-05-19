5a0f7235af972a07c4ab49c9364192f08b28fbf6
daehi8 <72012602+daehi8@users.noreply.github.com>
2021-05-19 16:35:47 +0900

    Add AlgorithmTest

---
+++ AlgorithmTest/substring01.java
@@ -0,0 +1,23 @@
+package study;
+
+public class substring01 {
+	
+//  가운데 글자 가져오기	
+//	단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+//	단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+//	재한사항
+//	s는 길이가 1 이상, 100이하인 스트링입니다.
+	
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        int subLength = (length-1)/2;
+        
+        if(length%2 == 1){
+            answer = s.substring(subLength,subLength+1);
+        }else if(length%2 == 0){
+            answer = s.substring(subLength,subLength+2);
+        }
+        return answer;
+    }
+}
